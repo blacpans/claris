b985ac6111688c22b303971ce78b48753da90503
blacpans <blacpans@gmail.com>
2026-01-20 23:00:22 +0900

    feat: retry webhook

--- src/hello.ts
+++ src/hello.ts
@@ -1,6 +1,7 @@
 export function hello(name: string): string {
   // TODO: Fix this intentionally bad code for review
   console.log("Debug log that should be removed");
+  console.log("Another debug log for retry"); // Trigger new event
   const greeting = "Hello " + name;
   return greeting;
 }
