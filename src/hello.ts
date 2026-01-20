b27542cc8da47c1ad74e2f2dfca3b22a14869d6e
blacpans <blacpans@gmail.com>
2026-01-20 22:55:30 +0900

    feat: trigger webhook test

---
+++ src/hello.ts
@@ -0,0 +1,6 @@
+export function hello(name: string): string {
+  // TODO: Fix this intentionally bad code for review
+  console.log("Debug log that should be removed");
+  const greeting = "Hello " + name;
+  return greeting;
+}
