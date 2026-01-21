459c41a2462f2d098c41d7784bfdac842922e2b7
blacpans <blacpans@gmail.com>
2026-01-21 09:02:30 +0900

    feat: retry webhook 9 - global location test

--- src/hello.ts
+++ src/hello.ts
@@ -1,7 +1,7 @@
 export function hello(name: string): string {
   // TODO: Fix this intentionally bad code for review
   console.log("Debug log that should be removed");
-  console.log("Retry 8: Final Test!"); // Trigger event 9
+  console.log("Retry 9: Global Location Test!"); // Trigger event 10
   const greeting = "Hello " + name;
   return greeting;
 }
